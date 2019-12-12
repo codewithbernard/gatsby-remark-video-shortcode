f28c730994bae87bb122d7e1711173cedd79caf5
Aayushi Agrawal <aayushi.a@go-jek.com>
2019-12-12 19:11:12 +0530

    fix: video preview

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-const visit = require("unist-util-visit")
-const _ = require(`lodash`)
-var unified = require("unified")
-var parse = require("remark-parse")
-var shortcodes = require("remark-shortcodes")
+const visit = require("unist-util-visit");
+const _ = require(`lodash`);
+var unified = require("unified");
+var parse = require("remark-parse");
+var shortcodes = require("remark-shortcodes");
 
-const extractString = str => {
-  return str.substring(1, str.length - 1)
-}
 module.exports = ({ markdownAST }, pluginOptions) => {
   visit(markdownAST, "paragraph", node => {
-    let shortcode = node.children[0]
+    let shortcode = node.children[0];
 
     if (shortcode.type === "text" && !!shortcode.value.match(/{{< Video /)) {
       var tree = unified()
@@ -17,32 +14,34 @@ module.exports = ({ markdownAST }, pluginOptions) => {
         // Plugin inserted below, with custom options for start/end blocks.
         .use(shortcodes, {
           startBlock: "{{<",
-          endBlock: ">}}",
+          endBlock: ">}}"
         })
         // Turn off position output for legibility below.
         .data("settings", { position: false })
-        .parse(shortcode.value)
-      let x = tree.children[0].attributes
-      const url = extractString(x.url ? _.escape(x.url) : "")
-      const control = extractString(x.control)
-      const loop = extractString(x.loop)
-      const autoplay = extractString(x.autoplay)
-      const muted = extractString(x.muted)
-      const height = extractString(x.height)
-      const width = extractString(x.width)
-      const poster = extractString(x.poster ? _.escape(x.poster) : "")
+        .parse(shortcode.value);
+      let x = tree.children[0].attributes;
+      const url = x.url ? _.escape(x.url) : "";
+      const control = x.control;
+      const loop = x.loop;
+      const autoplay = x.autoplay;
+      const muted = x.muted;
+      const height = x.height;
+      const width = x.width;
+      const poster = x.poster ? _.escape(x.poster) : "";
 
       const html = `
-      <div style=\"position: relative; margin-left: auto; margin-right: auto;\">\n <video ${control &&
-        `controls=${control}`} loop=${loop}\n ${autoplay &&
-        muted &&
-        `autoplay=${autoplay}`} allowfullscreen=true\n ${muted &&
-        `muted=${muted}`} height=${height} width=${width}  poster=${poster}>\n         <source src=${url} type=\"video/mp4\" />\n         <source src=${url} type=\"video/ogg\" />\n         <source src=${url} type=\"video/webm\" />\n       </video>\n </div>\n
-    `.trim()
-      node.type = "html"
-      node.children = undefined
-      node.value = html
+      <div style=\"position: relative; margin-left: auto; margin-right: auto;\">\n <video ${
+        control === "true" ? "controls" : ""
+      } ${loop === "true" ? "loop" : ""}\n ${
+        autoplay === "true" && muted === "true" ? "autoplay" : ""
+      } allowfullscreen=true\n ${
+        muted === "true" ? "muted=true" : ""
+      } height=${height} width=${width}  poster=${poster}>\n <source src=${url} type=\"video/mp4\" />\n <source src=${url} type=\"video/ogg\" />\n <source src=${url} type=\"video/webm\" />\n       </video>\n </div>\n
+    `.trim();
+      node.type = "html";
+      node.children = undefined;
+      node.value = html;
     }
-  })
-  return markdownAST
-}
+  });
+  return markdownAST;
+};
